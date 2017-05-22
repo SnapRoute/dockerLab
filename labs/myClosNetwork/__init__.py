4f91a539c0521dc3cc5907015d6dd70a5ddbd468
Andy Gossett <gossett.andy@gmail.com>
2017-05-22 14:53:08 -0400

    updated description for myClosNetwork

--- labs/myClosNetwork/__init__.py
+++ labs/myClosNetwork/__init__.py
@@ -1,5 +1,5 @@
 """
 myClosNetwork: An Example Custom Topology
 Create custom topology and staging files to emulate your network. See the
-documentation under the custom lab for more details
+documentation under the myClosNetwork lab for more details
 """

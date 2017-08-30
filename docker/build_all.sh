64421e14ef98b7af38cb9c78e85a0b0165ded3e8
Phillip Ferrell <pferrell@snaproute.com>
2017-08-30 13:57:54 -0700

    removing unnecessary file

--- docker/build_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-DOCKER_BUILD_WRAPPER=scripts/docker_build_wrapper.sh
-
-$DOCKER_BUILD_WRAPPER snaproute/labs device_base device_base/Dockerfile device_base/context/
-$DOCKER_BUILD_WRAPPER snaproute/labs client client/Dockerfile client/context/
-$DOCKER_BUILD_WRAPPER snaproute/labs sysadmin sysadmin/Dockerfile sysadmin/context/
